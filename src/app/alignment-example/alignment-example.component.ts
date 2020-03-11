d50e26909c3bd5a2d93c8af29940ee1c46c63d07
irfaan.fakir <irfaan@hotmail.co.za>
2020-03-12 00:51:43 +0200

    "Added other stylesheet."

--- src/app/alignment-example/alignment-example.component.ts
+++ src/app/alignment-example/alignment-example.component.ts
@@ -5,7 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './alignment-example.component.html',
   styleUrls: [
     './alignment-example.component.css',
-    './old-approach.css'
+    // './old-approach.css'
+    // './new-approach.css'
   ]
 })
 export class AlignmentExampleComponent implements OnInit {
