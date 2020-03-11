6d9f59c7807592cd6de6c3e70440dd378365e185
irfaan.fakir <irfaan@hotmail.co.za>
2020-03-11 15:53:14 +0200

    Added flexbox solution for cards.

--- src/app/ordinary-css/ordinary-css.component.ts
+++ src/app/ordinary-css/ordinary-css.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ordinary-css',
   templateUrl: './ordinary-css.component.html',
-  styleUrls: ['./ordinary-css.component.css']
+  styleUrls: [
+    './ordinary-css.component.css',
+  ]
 })
 export class OrdinaryCSSComponent implements OnInit {
 
